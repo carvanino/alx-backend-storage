be2e801d71e6ba9d7f7ed3d6deec7f3169b1e4d1
Akinola <akinolatofunmi04@gmail.com>
2023-04-07 02:39:53 +0000

    Fix order

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,12 @@
 -- ranked by their longevity
 -- COALESCE() && IFNULL() will return use the default value 2020 to coompute
 -- if split is NULL
-SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan FROM metal_bands 
-WHERE style LIKE '%Glam rock%' 
-ORDER BY lifespan DESC;
+SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan 
+FROM metal_bands 
+WHERE style LIKE '%Glam rock%'
+ORDER BY
+CASE WHEN (COALESCE(split, 2020) - formed) <> 0
+	THEN lifespan
+	ELSE 0
+END DESC,
+band_name DESC;
