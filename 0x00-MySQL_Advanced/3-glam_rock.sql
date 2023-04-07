61925fa69909c543b07491dd5eb4f9348ea95626
Akinola <akinolatofunmi04@gmail.com>
2023-04-07 02:46:11 +0000

    Fix?

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,10 @@
 -- lists all bands with Glam rock as their main style, 
 -- ranked by their longevity
--- COALESCE() && IFNULL() will return use the default value 2020 to coompute
--- if split is NULL
-SELECT band_name, (COALESCE(split, '2020') - formed) AS lifespan 
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan 
 FROM metal_bands 
 WHERE style LIKE '%Glam rock%'
 ORDER BY
-CASE WHEN (COALESCE(split, '2020') - formed) <> 0
+CASE WHEN (IFNULL(split, 2020) - formed) <> 0
 	THEN lifespan
 	ELSE 0
 END DESC,
