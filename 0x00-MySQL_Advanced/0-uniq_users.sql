bcb94be9d8a4f74c35955543441afbcb52648752
Akinola <akinolatofunmi04@gmail.com>
2023-04-07 00:52:18 +0000

    Iclude comment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,6 @@
+-- Creates a table users with unique column and an auto incremental
+-- primary key
+
 CREATE TABLE IF NOT EXISTS users (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 email VARCHAR(255) NOT NULL UNIQUE,
