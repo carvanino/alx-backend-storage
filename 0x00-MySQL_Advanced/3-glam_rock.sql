80164d04acd6d8a16195fac830f18afbb0d2c43e
Akinola <akinolatofunmi04@gmail.com>
2023-04-07 02:14:42 +0000

    Fix case for null value

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,7 @@
---  lists all bands with Glam rock as their main style, 
+-- lists all bands with Glam rock as their main style, 
 -- ranked by their longevity
-SELECT band_name, (split - formed) as lifespan FROM metal_bands 
+-- COALESCE() && IFNULL() will return use the default value 2020 to coompute
+-- if split is NULL
+SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan FROM metal_bands 
 WHERE style LIKE '%Glam rock%' 
 ORDER BY lifespan DESC;
