dd9564cec10af74e769bdd5b0fdb60bafd467eb1
Akinola <akinolatofunmi04@gmail.com>
2023-04-12 11:16:46 +0000

    Task 0 & 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-
+Reading from Redis by type
 """
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
+
 
 class Cache:
     """
@@ -13,7 +14,7 @@ class Cache:
     """
 
     def __init__(self, host='localhost', port=6379,
-            db=0, password=None) -> None:
+                 db=0, password=None) -> None:
         """
         Instantiates the class
         """
@@ -28,3 +29,30 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int]:
+        """
+        Converts a data to the desired format(str, int or byte)
+        """
+        if not self._redis.get(key):
+            return None
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """
+        Parametrize to str
+        """
+        # value = self._redis.get(str(key))
+        value = self.get(key, lambda d: d.decode('utf-8'))
+        return value
+
+    def get_int(self, key: str) -> int:
+        """
+        Parametrize to int
+        """
+        # key = self._redis.get(int(key))
+        value = self.get(key, int)
+        return value
